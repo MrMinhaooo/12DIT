c5966218718e02590a21ce9bc81171a432ecd5ab
hu.minha <hu.minha@awatapu.school.nz>
2021-07-07 12:45:36 +1200

    Not blank function

---
+++ 01_Movie_Fundraiser/01_not_blank.py
@@ -0,0 +1,16 @@
+# function goes here
+
+def not_blank(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+    while response != "":
+        return response
+    else:
+        print("This can't be be blank please enter a name")
+
+# main routine goes here
+
+name = not_blank("Name: ")
